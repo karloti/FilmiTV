c1495f7ef233ad0bf98596de044c18f7dbdc3c4d
Kaloyan Karaivanov <skarloti@gmail.com>
2021-08-06 19:12:38 +0300

    UI/UX

--- app/src/main/java/com/peshkunti/filmitv/MainActivity.kt
+++ app/src/main/java/com/peshkunti/filmitv/MainActivity.kt
@@ -2,20 +2,19 @@ package com.peshkunti.filmitv
 
 import android.content.ContentValues.TAG
 import android.content.res.AssetManager
-import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.*
+import androidx.compose.animation.core.InternalAnimationApi
+import androidx.compose.animation.core.snap
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,18 +22,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.peshkunti.filmitv.FilterState.*
+import com.peshkunti.filmitv.ViewState.*
 import com.peshkunti.filmitv.classes.Channel
 import com.peshkunti.filmitv.classes.Events
 import com.peshkunti.filmitv.classes.Movie
 import com.peshkunti.filmitv.classes.filterRange
 import com.peshkunti.filmitv.ui.theme.FilmiTVTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
-import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Duration
@@ -57,8 +53,20 @@ val MOUNTS = listOf<String>(
     "Декември",
 )
 
+private enum class ViewState {
+    Large,
+    Small,
+}
+
+private enum class FilterState {
+    NoFilter,
+    Efirni,
+}
+
+
 @ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
+    @InternalAnimationApi
     @ExperimentalFoundationApi
     @ExperimentalTime
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,7 +96,6 @@ fun start(assetManager: AssetManager) {
     }
 }
 
-@ExperimentalFoundationApi
 @Suppress("UNCHECKED_CAST")
 @ExperimentalTime
 @ExperimentalAnimationApi
@@ -112,8 +119,11 @@ fun MovieContent() {
                     Channel.NOVA_TV
                 )
         }
-    var buttonEfirni by remember { mutableStateOf(false) }
-    var buttonExtend by remember { mutableStateOf(false) }
+    var viewState by remember { mutableStateOf(Small) }
+    var filterState by remember { mutableStateOf(NoFilter) }
+
+//    val transitionViewState = updateTransition(targetState = viewState, label = "")
+//    val transitionFilterState = updateTransition(targetState = filterState, label = "")
 
     Column {
         Row(
@@ -121,43 +131,55 @@ fun MovieContent() {
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Button(
-                onClick = { buttonExtend = !buttonExtend },
+                onClick = {
+                    viewState = when (viewState) {
+                        Large -> Small
+                        Small -> Large
+                    }
+                },
                 modifier = Modifier.padding(16.dp),
                 shape = MaterialTheme.shapes.medium,
             ) {
                 Text(
                     text = "РАЗШИЕРНИ",
                     modifier = Modifier.padding(4.dp),
-                    color = if (buttonExtend)
-                        MaterialTheme.colors.background
-                    else
-                        MaterialTheme.colors.onBackground,
+                    color = when (viewState) {
+                        Large -> MaterialTheme.colors.onBackground
+                        Small -> MaterialTheme.colors.background
+                    }
                 )
             }
             Button(
-                onClick = { buttonEfirni = !buttonEfirni },
+                onClick = {
+                    filterState = when (filterState) {
+                        NoFilter -> Efirni
+                        Efirni -> NoFilter
+                    }
+                },
                 modifier = Modifier.padding(16.dp),
                 shape = MaterialTheme.shapes.medium,
             ) {
                 Text(
                     text = "ЕФИРНИ",
                     modifier = Modifier.padding(4.dp),
-                    color = if (buttonEfirni)
-                        MaterialTheme.colors.background
-                    else
-                        MaterialTheme.colors.onBackground,
+                    color = when (filterState) {
+                        NoFilter -> MaterialTheme.colors.background
+                        Efirni -> MaterialTheme.colors.onBackground
+                    }
                 )
             }
         }
         LazyColumn(
-            modifier = Modifier.padding(horizontal = 16.dp)
-        ) {
+            modifier = Modifier.padding(horizontal = 16.dp),
+            reverseLayout = false,
+
+            ) {
             items(movies.size) { index ->
                 Poster(
                     movie = movies[index],
                     number = index,
-                    filterEfirni = buttonEfirni,
-                    largeView = buttonExtend
+                    filterState = filterState,
+                    viewState = viewState
                 )
             }
         }
@@ -167,150 +189,164 @@ fun MovieContent() {
 @ExperimentalTime
 @ExperimentalAnimationApi
 @Composable
-fun Poster(movie: Movie, number: Int, filterEfirni: Boolean, largeView: Boolean) {
-    val typography: Typography = MaterialTheme.typography
-    Card {
-        AnimatedVisibility(
-            visible = !(filterEfirni && movie.hidden),
-        ) {
-            if (largeView)
-                Column(modifier = Modifier.padding(vertical = 8.dp)) {
-                    CardImage(largeView, number)
-                    Spacer(modifier = Modifier.height(16.dp))
-                    CardText(movie, typography)
-                }
-            else
-                Row(modifier = Modifier.padding(vertical = 8.dp)) {
-                    CardImage(largeView, number)
-                    Spacer(modifier = Modifier.width(16.dp))
-                    CardText(movie, typography)
-                }
-        }
+private fun Poster(movie: Movie, number: Int, filterState: FilterState, viewState: ViewState) {
+//    val transition = updateTransition(targetState = viewState, label = "")
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    isExpanded = when (viewState) {
+        Large -> true
+        Small -> false
     }
 
-}
+    var text by remember { mutableStateOf("") }
 
-@Composable
-private fun CardImage(
-    expanded: Boolean,
-    number: Int
-) {
-    if (expanded)
-        Column(
-            Modifier.fillMaxWidth()
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.header),
-                contentDescription = null,
-                contentScale = ContentScale.FillWidth,
-                alignment = Alignment.TopStart,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .animateContentSize()
-            )
-        }
-    else
-        Column {
-            Image(
-                painter = painterResource(id = R.drawable.header),
-                contentDescription = null,
-                contentScale = ContentScale.Inside,
-                alignment = Alignment.TopStart,
-                modifier = Modifier
-                    .height(82.dp)
-                    .animateContentSize()
-            )
+    AnimatedVisibility(
+        modifier = Modifier.padding(4.dp),
+        visible = when (filterState) {
+            NoFilter -> true
+            Efirni -> !movie.hidden
+        },
+        enter = expandVertically(),
+        exit = shrinkVertically(),
+    ) {
+        Surface(
+            modifier = Modifier
+                .wrapContentWidth()
+                .animateContentSize(
+                    animationSpec = snap(0),
+                    finishedListener = { a, b -> Log.d(TAG, "Poster: ${a.height} ${b.height} ") }
+                ),
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colors.surface,
+            elevation = 1.dp,
+            border = BorderStroke(
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
+                width = 1.dp
+            ),
+
+            ) {
+            AnimatedContent(
+                targetState = viewState,
+                contentAlignment = Alignment.TopStart,
+            ) { state ->
+                Image(
+                    modifier = when (state) {
+                        Large -> Modifier.fillMaxWidth()
+                        Small -> Modifier.width(62.dp)
+                    },
+                    painter = painterResource(id = R.drawable.header),
+                    contentDescription = null,
+                    contentScale = when (state) {
+                        Large -> ContentScale.FillWidth
+                        Small -> ContentScale.Inside
+                    },
+                    alignment = when (viewState) {
+                        Large -> Alignment.TopCenter
+                        Small -> Alignment.BottomCenter
+                    },
+                )
+            }
+
             Text(
                 text = "${number + 1}",
                 style = MaterialTheme.typography.body2,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
             )
-        }
-}
+            Box(
+                modifier = Modifier
+                    .run {
+                        when (viewState) {
+                            Large -> padding(
+                                start = 8.dp,
+                                end = 8.dp,
+                                bottom = 8.dp,
+                                top = 540.dp
+                            )
+                            Small -> padding(
+                                start = 72.dp,
+                                end = 8.dp,
+                                bottom = 8.dp,
+                            )
+                        }
+                    },
+                contentAlignment = Alignment.BottomEnd,
+            )
+            {
+                text = (Clock.System.now() - movie.dataTime).inWholeDays
+                    .let {
+                        when (it) {
+                            0L -> "днес"
+                            1L -> "вчера"
+                            else -> "преди $it дни"
+                        }
+                    }
+                val localDate by remember {
+                    mutableStateOf(movie.dataTime.toLocalDateTime(TimeZone.currentSystemDefault()))
+                }
+                val dateTime by remember {
+                    mutableStateOf(localDate.date.run { "$dayOfMonth ${MOUNTS[monthNumber]} $year" } + " от " +
+                            localDate.toString().substringAfter("T"))
+                }
 
-@ExperimentalAnimationApi
-@ExperimentalTime
-@Composable
-private fun CardText(
-    movie: Movie,
-    typography: Typography
-) {
-    var isExpanded by remember { mutableStateOf(false) }
-    var text by remember { mutableStateOf("") }
-    text = (Clock.System.now() - movie.dataTime).inWholeDays
-        .let {
-            when (it) {
-                0L -> "днес"
-                1L -> "преди $it ден"
-                else -> "преди $it дни"
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .clickable { isExpanded = !isExpanded }
+                ) {
+                    Text(
+                        text = movie.title,
+                        style = typography.h6,
+                        color = if (Clock.System.now() in
+                            movie.dataTime..movie.dataTime + Duration.hours(3)
+                        ) MaterialTheme.colors.secondary else MaterialTheme.colors.primary,
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier
+                            .fillMaxWidth()
+
+                    )
+                    Text(
+                        "жанрове: " + movie.genres?.joinToString { it.names.toList()[1] },
+                        style = typography.body2
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("година: " + movie.year, style = typography.body2)
+                        Text(
+                            text = text,
+                            style = typography.body2,
+                            textAlign = TextAlign.End,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        Arrangement.SpaceBetween
+                    ) {
+
+                        Text(
+                            text = movie.channel.toString(),
+                            style = typography.body2,
+                        )
+                        Text(
+                            text = dateTime,
+                            style = typography.body2,
+                        )
+                    }
+                    AnimatedVisibility(visible = isExpanded) {
+                        Column(Modifier.padding(vertical = 8.dp)) {
+                            Text("информация: " + movie.presentation, style = typography.body2)
+                        }
+                    }
+                }
             }
         }
-    val localDate by remember {
-        mutableStateOf(movie.dataTime.toLocalDateTime(TimeZone.currentSystemDefault())) }
-
-    val dateTime by remember {
-        mutableStateOf(localDate.date.run { "$dayOfMonth ${MOUNTS[monthNumber]} $year" } + " от " +
-                localDate.toString().substringAfter("T"))
-    }
-
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .clickable { isExpanded = !isExpanded }
-    ) {
-        Text(
-            text = movie.title,
-            style = MaterialTheme.typography.h6,
-            color = if (Clock.System.now() in
-                movie.dataTime..movie.dataTime + Duration.hours(3)
-            ) MaterialTheme.colors.secondary else MaterialTheme.colors.primary,
-            maxLines = if (isExpanded) Int.MAX_VALUE else 2,
-            overflow = TextOverflow.Ellipsis,
-            modifier = Modifier
-                .fillMaxWidth()
-
-        )
-        Text(
-            "жанрове: " + movie.genres?.joinToString { it.names.toList()[1] },
-            style = typography.body2
-        )
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("година: " + movie.year, style = typography.body2)
-            Text(
-                text = text,
-                style = typography.body2,
-                textAlign = TextAlign.End,
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            Arrangement.SpaceBetween
-        ) {
-
-
-            Text(
-                text = movie.channel.toString(),
-                style = typography.body2,
-            )
-            Text(
-                text = dateTime,
-                style = typography.body2,
-            )
-
-        }
-        AnimatedVisibility(visible = isExpanded) {
-            Column(Modifier.padding(vertical = 8.dp)) {
-                Text("информация: " + movie.presentation, style = typography.body2)
-            }
-        }
     }
 }
 
-
+/*
 @ExperimentalFoundationApi
 @OptIn(ExperimentalAnimationApi::class)
 @ExperimentalTime
@@ -347,4 +383,4 @@ fun PreviewPoster() {
             )
         }
     }
-}
+}*/
